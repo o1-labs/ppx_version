de004074a84c18dc5c1d9af6b3caff2135f3584e
Paul Steckler <steck@stecksoft.com>
2020-06-17 17:28:00 -0700

    dummy derivers may take arguments

--- src/dummy_derivers.ml
+++ src/dummy_derivers.ml
@@ -1,26 +1,37 @@
-(* dummy_derivers.ml -- no-op derivers for aliasing *)
+(* dummy_derivers.ml -- create no-op derivers *)
 
 open Core_kernel
 
-let str_type_decl =
-  let deriver ~loc:_ ~path:_ (_rec_flag, _type_decls) = [] in
-  Ppxlib.Deriving.Generator.make_noarg deriver
+(* type declarations *)
+let type_decl_gen0 ~loc:_ ~path:_ _(_rec_flag,_type_decls) = []
+let type_decl_gen1 ~loc:_ ~path:_ _(_rec_flag,_type_decls) _ = []
+let type_decl_gen2 ~loc:_ ~path:_ _(_rec_flag,_type_decls) _ _ = []
+let type_decl_gen3 ~loc:_ ~path:_ _(_rec_flag,_type_decls) _ _ _ = []
+let type_decl_gen4 ~loc:_ ~path:_ _(_rec_flag,_type_decls) _ _ _ _ = []
 
-let str_type_ext =
-  let deriver ~loc:_ ~path:_ _type_ext = [] in
-  Ppxlib.Deriving.Generator.make_noarg deriver
+let add_type_decl_no_op name args generator =
+  let str_type_decl = Ppxlib.Deriving.Generator.make args generator in
+  Ppxlib.Deriving.add name ~str_type_decl |> Ppxlib.Deriving.ignore
 
-let type_decl_dummy_deriver =
-  Ppxlib.Deriving.add "dummy_type_decl" ~str_type_decl
+let add_type_decl_no_op0 name args = add_type_decl_no_op name args type_decl_gen0
+let add_type_decl_no_op1 name args = add_type_decl_no_op name args type_decl_gen1
+let add_type_decl_no_op2 name args = add_type_decl_no_op name args type_decl_gen2
+let add_type_decl_no_op3 name args = add_type_decl_no_op name args type_decl_gen3
+let add_type_decl_no_op4 name args = add_type_decl_no_op name args type_decl_gen4
 
-let type_ext_dummy_deriver = Ppxlib.Deriving.add "dummy_type_ext" ~str_type_ext
+(* type extensions *)
+let type_ext_gen0 ~loc:_ ~path:_ _type_ext = []
+let type_ext_gen1 ~loc:_ ~path:_ _type_ext _ = []
+let type_ext_gen2 ~loc:_ ~path:_ _type_ext _ _ = []
+let type_ext_gen3 ~loc:_ ~path:_ _type_ext _ _ _ = []
+let type_ext_gen4 ~loc:_ ~path:_ _type_ext _ _ _ _ = []
 
-let add_type_decl_aliases aliases =
-  List.iter aliases ~f:(fun alias ->
-      Ppxlib.Deriving.add_alias alias [type_decl_dummy_deriver]
-      |> Ppxlib.Deriving.ignore )
+let add_type_ext_no_op name args generator =
+  let str_type_ext = Ppxlib.Deriving.Generator.make args generator in
+  Ppxlib.Deriving.add name ~str_type_ext |> Ppxlib.Deriving.ignore
 
-let add_type_ext_aliases aliases =
-  List.iter aliases ~f:(fun alias ->
-      Ppxlib.Deriving.add_alias alias [type_ext_dummy_deriver]
-      |> Ppxlib.Deriving.ignore )
+let add_type_ext_no_op0 name args = add_type_ext_no_op name args type_ext_gen0
+let add_type_ext_no_op1 name args = add_type_ext_no_op name args type_ext_gen1
+let add_type_ext_no_op2 name args = add_type_ext_no_op name args type_ext_gen2
+let add_type_ext_no_op3 name args = add_type_ext_no_op name args type_ext_gen3
+let add_type_ext_no_op4 name args = add_type_ext_no_op name args type_ext_gen4
--- src/print_versioned_types.ml
+++ src/print_versioned_types.ml
@@ -2,5 +2,9 @@
 
 let () =
   Ppx_version.Versioned_type.set_printing () ;
-  Ppx_version.Dummy_derivers.add_type_ext_aliases ["register_event"] ;
+  let register_event_args =
+    let open Ppxlib.Deriving.Args in
+    empty +> arg "msg" __
+  in
+  Ppx_version.Dummy_derivers.add_type_ext_no_op1 "register_event" register_event_args;
   Ppxlib.Driver.standalone ()
