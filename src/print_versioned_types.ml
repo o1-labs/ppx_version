8d83599dfd35adb7b31a490629493f0778ed868e
Paul Steckler <steck@stecksoft.com>
2020-07-08 17:53:56 -0700

    dhall_type is for type decls

--- src/print_versioned_types.ml
+++ src/print_versioned_types.ml
@@ -4,5 +4,5 @@ let () =
   Ppx_version.Versioned_type.set_printing () ;
   let register_event_arg = Ppxlib.Deriving.Args.(arg "msg" __) in
   Ppx_version.Dummy_derivers.add_type_ext1 "register_event" register_event_arg;
-  Ppx_version.Dummy_derivers.add_type_ext "dhall_type";
+  Ppx_version.Dummy_derivers.add_type_decl "dhall_type";
   Ppxlib.Driver.standalone ()
